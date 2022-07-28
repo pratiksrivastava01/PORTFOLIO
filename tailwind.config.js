8c8a4a0fb80459c0559efa372cc2264f90727638
Pratik <srivastavapratik35@gmail.com>
2022-07-29 00:48:34 +0530

    😃😃created the basic design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -6,35 +5,33 @@ module.exports = {
   ],
   theme: {
     extend: {
-      extend: {
-        colors: {
-          primary: {
-            DEFAULT: "#01080E",
-            light: "#011627",
-            lighter: "#011627",
-          },
-          secondary: {
-            DEFAULT: "#607B96",
-            blue: "#3C9D93",
-            purple: "#4D5BCE",
-          },
-          accent: {
-            DEFAULT: "#FEA55F",
-            green: "#43D9AD",
-            orange: "#E99287",
-            pink: "#C98BDF",
-          },
-          gradients: {
-            DEFAULT: "#4D5BCE",
-            secondary: "#43D9AD",
-          },
-          lines: "#1E2D3D",
+      colors:{
+        primary:{
+          DEFAULT:"#01080E",
+          light:"#011627",
+          lighter:"#011627"
         },
-        fontFamily: {
-          headline: "Fira Code, monospace",
+        secondary:{
+          DEFAULT:"#607B96",
+          blue:"#3C9D93",
+          purple:"#4D5BCE",
         },
+        accent:{
+          DEFAULT:"#FEA55F",
+          green:"#43D9AD",
+          orange:"#E99287",
+          pink:"#C98BDF"
+        },
+        gradients:{
+          DEFAULT:"#4D5BCE",
+          secondary:"#43D9AD"
+        },
+        lines:"#1E2D3D"
       },
+      fontFamily:{
+        headline:"Fira Code, monospace"
+      }
     },
   },
   plugins: [],
-};
+}
