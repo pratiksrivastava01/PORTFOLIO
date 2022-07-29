5aa439232f8b69c22716769ca975b3b4f85ba93e
Pratik <srivastavapratik35@gmail.com>
2022-07-29 10:41:51 +0530

    🤗🤗designed the outline of index page

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,19 +1,87 @@
-import React from 'react'
-import { PageTitle } from '../components/PageTitle'
+import React from "react";
+import { PageTitle } from "../components/PageTitle";
+import Link from "next/link";
+import { SocialIcon } from "./SocialIcon";
 
-export const Layout = ({children}) => {
+export const Layout = ({ children }) => {
+  const navbar = [
+    {
+      title: "pratik-srivastava",
+      userName: true,
+      lastItem: false,
+    },
+    {
+      title: "hello",
+      userName: false,
+      lastItem: false,
+    },
+    {
+      title: "about",
+      userName: false,
+      lastItem: false,
+    },
+    {
+      title: "projects",
+      userName: false,
+      lastItem: false,
+    },
+    {
+      title: "contact",
+      userName: false,
+      lastItem: true,
+    },
+  ];
+  const social = [
+    {
+      link: "https://www.linkedin.com/in/srivastava2191/",
+      icon: <i className="ri-linkedin-fill"></i>,
+    },
+    {
+      icon: <i className="ri-instagram-fill"></i>,
+    },
+    {
+      link: "https://twitter.com/Pratik_Srivasta",
+      icon: <i className="ri-twitter-fill"></i>,
+    },
+    // {
+    //   icon: <i className="ri-facebook-fill"></i>,
+    // },
+  ];
   return (
-    <div className="bg-primary w-full h-screen flex justify-center items-center">
-        <div className="w-11/12 bg-primary-lighter rounded-lg border-lines border-2" style={{height:"90vh"}}>
+    <div className=" bg-primary w-full h-screen flex justify-center items-center">
+      <div className="w-11/12 relative bg-primary-lighter rounded-lg overflow-hidden shadow-md shadow-primary-lighter border-lines border-2 h-90per">
         <div className="border-b-2 border-b-lines flex">
-          <PageTitle pageTitle={"pratik-srivastava"} userName={true}/>
-          <PageTitle pageTitle={"hello"}/>
-          <PageTitle pageTitle={"about-me"}/>
-          <PageTitle pageTitle={"projects"}/>
-          <PageTitle pageTitle={"contact-me"} contact={true}/>
+          {navbar.map((page) => (
+            <PageTitle
+              key={page.title}
+              pageTitle={page.title}
+              userName={page.userName}
+              lastItem={page.lastItem}
+            />
+          ))}
         </div>
-            {children}
+        <div className="max-h-85per h-85per overflow-auto">{children}</div>
+        <div className="border-t-2 absolute border-t-lines flex w-full">
+          <Link href="">
+            <a className=" font-headline text-secondary text-xs inline-block border-r-2 hover:text-white border-r-lines px-2 py-3 hover:border-b-accent border-b-primary-lighter transition border-b-2">
+              <h1 className=" mt-1">find me on:</h1>
+            </a>
+          </Link>
+          {social.map((icon) => (
+            <SocialIcon key={icon.icon} icon={icon.icon} link={icon.link} />
+          ))}
+          <a
+            target="_blank"
+            href={"https://github.com/pratiksrivastava01"}
+            className="text-secondary hover:text-white text-headline  flex items-center last:ml-auto border-r-0 border-l-2 border-l-lines inline-block px-3  py-3 hover:border-b-accent border-b-primary-lighter transition border-b-2  "
+          >
+            <span className="text-xs block mr-1">@pratiksrivastava01</span>
+            <i className="ri-github-fill"></i>
+          </a>
         </div>
-    </div>    
-  )
-}
+      </div>
+    </div>
+  );
+};
+
+
